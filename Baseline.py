d5b6e06ebe53edb901e94cd20e565f80d8c453c7
Sigridlind <sigrid.s.lind@gmail.com>
2026-03-23 13:07:40 +0100

    added class vocab

---
+++ Baseline.py
@@ -0,0 +1,25 @@
+class Vocab():
+    def __init__(self, pad_unk='<PAD>'):
+        """
+        A convenience class that can help store a vocabulary
+        and retrieve indices for inputs.
+        """
+        self.pad_unk = pad_unk
+        self.word2idx = {self.pad_unk: 0}
+        self.idx2word = [self.pad_unk]
+
+    def getIdx(self, word, add=False):
+        if word not in self.word2idx:
+            if add:
+                self.word2idx[word] = len(self.idx2word)
+                self.idx2word.append(word)
+            else:
+                return self.word2idx[self.pad_unk]
+        return self.word2idx[word]
+
+    def getWord(self, idx):
+        return self.idx2word[idx]
+
+
+
+max_len= max([len(x[0]) for x in train_data ])
